25817bd5bc69ac0a77092f70887dd113a5f68074
Tshephang <zinjie7@gmail>
2025-07-30 04:14:46 +0200

    commented out un-used PDF code

--- lib/screens/resources/file_actions.dart
+++ lib/screens/resources/file_actions.dart
@@ -1,15 +1,15 @@
-import 'package:url_launcher/url_launcher.dart';
+// import 'package:url_launcher/url_launcher.dart';
 
-import 'file_model.dart';
+// import 'file_model.dart';
 
-// Function to open a PDF file from a URL
-Future<void> openFile(PDFDocument doc) async {
-  final Uri fileUrl = Uri.parse(doc.url); // Convert the URL string to a Uri
+// // Function to open a PDF file from a URL
+// Future<void> openFile(PDFDocument doc) async {
+//   final Uri fileUrl = Uri.parse(doc.url); // Convert the URL string to a Uri
 
-  if (await canLaunchUrl(fileUrl)) {
-    await launchUrl(fileUrl, mode: LaunchMode.externalApplication); // Opens in the default browser or app
-  } else {
-    print("Could not launch $fileUrl");
-    // Optionally, show a snackbar or dialog to notify the user
-  }
-}
+//   if (await canLaunchUrl(fileUrl)) {
+//     await launchUrl(fileUrl, mode: LaunchMode.externalApplication); // Opens in the default browser or app
+//   } else {
+//     print("Could not launch $fileUrl");
+//     // Optionally, show a snackbar or dialog to notify the user
+//   }
+// }
